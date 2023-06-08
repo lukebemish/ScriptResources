d1366f080b6d4f592407665cb1e3de7ff1a8f2be
Luke Bemish <lukebemish@lukebemish.dev>
2023-06-07 19:28:16 -0600

    Add listener on forge

--- Forge/src/main/groovy/dev/lukebemish/scriptresources/impl/forge/ScriptResourcesForge.groovy
+++ Forge/src/main/groovy/dev/lukebemish/scriptresources/impl/forge/ScriptResourcesForge.groovy
@@ -8,12 +8,17 @@ package dev.lukebemish.scriptresources.impl.forge
 import com.matyrobbrt.gml.GMod
 import dev.lukebemish.scriptresources.impl.Constants
 import dev.lukebemish.scriptresources.impl.ScriptResourcesCommon
+import dev.lukebemish.scriptresources.impl.ScriptResourcesListener
 import groovy.transform.CompileStatic
+import net.minecraftforge.event.AddReloadListenerEvent
 
 @GMod(Constants.MOD_ID)
 @CompileStatic
 class ScriptResourcesForge {
     ScriptResourcesForge() {
         ScriptResourcesCommon.init()
+        forgeBus.addListener(AddReloadListenerEvent) { event ->
+            event.addListener(new ScriptResourcesListener())
+        }
     }
 }
